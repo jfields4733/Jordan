2060b65e5193e3d7ab92763ef80422f2f453d803
jfields4733 <47197568+jfields4733@users.noreply.github.com>
2019-05-13 17:00:48 -0400

    Update LED Cube

--- LEDCube/LEDCube6/LEDCube6.runs/synth_1/eightPlanes.tcl
+++ LEDCube/LEDCube6/LEDCube6.runs/synth_1/eightPlanes.tcl
@@ -17,26 +17,23 @@ proc create_report { reportName command } {
     send_msg_id runtcl-5 warning "$msg"
   }
 }
-set_param synth.incrementalSynthesisCache C:/Users/SET253~1.HCC/AppData/Local/Temp/.Xil_SET253-04U/Vivado-12972-SET253-04C/incrSyn
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 create_project -in_memory -part xc7a100tcsg324-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/SET253-04U.HCCMAIN/LEDCube6/LEDCube6.cache/wt [current_project]
-set_property parent.project_path C:/Users/SET253-04U.HCCMAIN/LEDCube6/LEDCube6.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.cache/wt [current_project]
+set_property parent.project_path C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property board_part digilentinc.com:nexys4_ddr:part0:1.1 [current_project]
-set_property ip_output_repo c:/Users/SET253-04U.HCCMAIN/LEDCube6/LEDCube6.cache/ip [current_project]
+set_property ip_output_repo c:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 read_verilog -library xil_defaultlib {
-  C:/Users/SET253-04U.HCCMAIN/LEDCube6/LEDCube6.srcs/sources_1/new/DeMux.v
-  C:/Users/SET253-04U.HCCMAIN/LEDCube6/LEDCube6.srcs/sources_1/new/Latch.v
-  C:/Users/SET253-04U.HCCMAIN/LEDCube6/LEDCube6.srcs/sources_1/new/onePlane.v
-  C:/Users/SET253-04U.HCCMAIN/LEDCube6/LEDCube6.srcs/sources_1/new/main.v
+  C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.srcs/sources_1/new/DeMux.v
+  C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.srcs/sources_1/new/Latch.v
+  C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.srcs/sources_1/new/onePlane.v
+  C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.srcs/sources_1/new/main.v
 }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -46,8 +43,8 @@ read_verilog -library xil_defaultlib {
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/Nexys4DDR_Master.xdc
-set_property used_in_implementation false [get_files C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/Nexys4DDR_Master.xdc]
+read_xdc C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.srcs/constrs_1/new/Nexys4DDR_Master.xdc
+set_property used_in_implementation false [get_files C:/Users/SET253-04U.HCCMAIN/Documents/GitHub/Jordan-s-ENES246/LEDCube/LEDCube6/LEDCube6.srcs/constrs_1/new/Nexys4DDR_Master.xdc]
 
 set_param ips.enableIPCacheLiteLoad 1
 close [open __synthesis_is_running__ w]
